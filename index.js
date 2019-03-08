7bb6733a3ba97599a43fe183969e28b5bc038668
Fernando Caamano <Fernando.Caamano@sprint.com>
2019-03-08 11:57:22 -0300

    Add compression, bodyparser and morgan middlewares to express

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const compression = require('compression')
+const bodyParser = require('body-parser');
+const morgan = require('morgan')
+
 const app = express();
+const auth = require('./lib/auth');
 
 const port = process.env.PORT || 3000;
+
+app.use(compression());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(morgan('dev'));
 
 app.use('/api/v1/user', require('./routes/user').router());
 app.use('/api/v1/article', require('./routes/article').router());
