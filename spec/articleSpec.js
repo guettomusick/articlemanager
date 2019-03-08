1cb2861666acaf245ba2bcafbd77049914da6df1
Fernando Caamano <Fernando.Caamano@sprint.com>
2019-03-08 12:15:19 -0300

    Update specs to include auth and check cases witout or wrong auth

--- spec/articleSpec.js
+++ spec/articleSpec.js
@@ -2,16 +2,56 @@ const request = require('request-promise');
 require('../index.js');
 
 const baseUrl = 'http://localhost:3000/api/v1/article';
+const authKey = '5CD4ED173E1C95FE763B753A297D5';
+const headers = {
+  'x-access-token': authKey
+}
 
-describe("articleManager API - Article Routes", () => {
-  it("should return 200 when posting new article", async (done) => {
+describe('articleManager API - Article Routes', () => {
+  it('should return 403 if no token is provided', async(done) => {
+    try {
+      await request.get(baseUrl, {
+        qs: {tags: 'Tag1'},
+        json: true
+      });
+      fail('not 403');
+    } catch(err) {
+      expect(err.statusCode).toBe(403);
+    } finally {
+      done();
+    }
+  });
+
+  it('should return 403 if wrong token is provided', async(done) => {
+    try {
+      await request.get(baseUrl, {
+        qs: {tags: 'Tag1'},
+        json: true,
+        headers: {
+          'x-access-token': 'WRONG'
+        }
+      });
+      fail('not 403');
+    } catch(err) {
+      expect(err.statusCode).toBe(403);
+    } finally {
+      done();
+    }
+  });
+
+  it('should return 200 when posting new article', async (done) => {
     try {
       await request.post(baseUrl, {
+        method: 'POST',
+        body: {
           userId: 1,
           title: 'An Artcile',
           text: 'Praesent tempor vitae nisl eu pellentesque. Donec in lorem vitae ligula convallis laoreet.',
           tags: ['Tag1']
-        });
+        },
+        json: true,
+        headers
+      });
     } catch(err) {
       fail('not 200');
     } finally {
@@ -19,9 +59,13 @@ describe("articleManager API - Article Routes", () => {
     }
   });
 
-  it("should return 200 when getting all articles", async (done) => {
+  it('should return 200 when getting all articles', async (done) => {
     try {
-      await request.get(baseUrl, {qs: 'Tag1', json: true});
+      await request.get(baseUrl, {
+        qs: {tags: 'Tag1'},
+        json: true,
+        headers
+      });
     } catch(err) {
       fail('not 200');
     } finally {
@@ -29,14 +73,19 @@ describe("articleManager API - Article Routes", () => {
     }
   });
 
-  it("should return 200 when updating an article", async (done) => {
+  it('should return 200 when updating an article', async (done) => {
     try {
       await request.put(`${baseUrl}/1`, {
+        method: 'PUT',
+        body: {
           userId: 1,
           title: 'An Artcile 2',
           text: 'Praesent tempor vitae nisl eu pellentesque. Donec in lorem vitae ligula convallis laoreet.',
           tags: ['Tag1', 'Tag2']
-        });
+        },
+        json: true,
+        headers
+      });
     } catch(err) {
       fail('not 200');
     } finally {
@@ -44,9 +93,9 @@ describe("articleManager API - Article Routes", () => {
     }
   });
 
-  it("should return 200 when deleting an article", async (done) => {
+  it('should return 200 when deleting an article', async (done) => {
     try {
-      await request.delete(`${baseUrl}/1`);
+      await request.delete(`${baseUrl}/1`, { headers });
     } catch(err) {
       fail('not 200');
     } finally {
--- spec/userSpec.js
+++ spec/userSpec.js
@@ -2,14 +2,56 @@ const request = require('request-promise');
 require('../index.js');
 
 const baseUrl = 'http://localhost:3000/api/v1/user';
+const authKey = '5CD4ED173E1C95FE763B753A297D5';
+const headers = {
+  'x-access-token': authKey
+}
 
 describe("articleManager API - User Routes", () => {
+  it('should return 403 if no token is provided', async(done) => {
+    try {
+      await request.post(baseUrl, {
+        name: 'Test User',
+        avatar: 'https://api.adorable.io/avatars/285/abott@adorable.png'
+      });
+      fail('not 403');
+    } catch(err) {
+      expect(err.statusCode).toBe(403);
+    } finally {
+      done();
+    }
+  });
+
+  it('should return 403 if wrong token is provided', async(done) => {
+    try {
+      await request.post(baseUrl, {
+        body: {
+          name: 'Test User',
+          avatar: 'https://api.adorable.io/avatars/285/abott@adorable.png'
+        },
+        json: true,
+        headers: {
+          'x-access-token': 'WRONG'
+        }
+      });
+      fail('not 403');
+    } catch(err) {
+      expect(err.statusCode).toBe(403);
+    } finally {
+      done();
+    }
+  });
+
   it("should return 200 when posting new user", async (done) => {
     try {
       await request.post(baseUrl, {
+        body: {
           name: 'Test User',
           avatar: 'https://api.adorable.io/avatars/285/abott@adorable.png'
-        });
+        },
+        json: true,
+        headers
+      });
     } catch(err) {
       fail('not 200');
     } finally {
