8b676fc60b082fe0cee557d219ccca700bebd955
Fernando Caamano <Fernando.Caamano@sprint.com>
2019-03-08 02:11:22 -0300

    Create user routes and unit testing for that routes

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use('/api/v1/user', require('./routes/user').router());
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
